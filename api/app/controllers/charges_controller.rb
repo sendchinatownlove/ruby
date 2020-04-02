2cced99666d8dc41795c67ca5655f9ed6f56c9c0
Justin McKibben <justintmckibben@gmail.com>
2020-04-02 16:13:46 -0500

    Allow line_item.amount to be int
    Previously it would only accept a string

--- api/app/controllers/charges_controller.rb
+++ api/app/controllers/charges_controller.rb
@@ -26,7 +26,7 @@ class ChargesController < ApplicationController
       json_response(session)
     rescue Stripe::StripeError => e
       json_response(e.error.message, e.http_status)
-    rescue ActionController::ParameterMissing, InvalidLineItem=> e
+    rescue ActionController::ParameterMissing, InvalidLineItem => e
       json_response(e.message, :unprocessable_entity)
     rescue InvalidLineItem => e
       json_response(e.message, :unprocessable_entity)
@@ -54,8 +54,8 @@ class ChargesController < ApplicationController
       raise InvalidLineItem.new "line_item must be named `Gift Card` or `Donation`"
     end
 
-    amount_is_valid_int = line_item['amount'] == line_item['amount'].to_i.to_s
-    unless amount_is_valid_int
+    convertable_to_int = line_item['amount'] == line_item['amount'].to_i.to_s
+    unless line_item['amount'].is_a? Integer || convertable_to_int
       raise InvalidLineItem.new 'line_item.amount must be an Integer'
     end
 
