89b3e29b66a97bc721b778b58d1097927f66a9de
Justin McKibben <justintmckibben@gmail.com>
2020-04-04 23:02:13 -0500

    Update owner_url to owner_image_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_005514) do
+ActiveRecord::Schema.define(version: 2020_04_05_035845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_04_05_005514) do
     t.boolean "accept_donations"
     t.boolean "sell_gift_cards"
     t.string "owner_name"
-    t.string "owner_url"
+    t.string "owner_image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
