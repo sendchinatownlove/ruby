8a9e5fcd9a7292bb925ee049e57a909f5996db3f
Stanley Zheng <stanley@zheng.nyc>
2020-04-05 20:41:11 -0400

    addding bundle

--- docker_bootstrap.sh
+++ docker_bootstrap.sh
@@ -3,7 +3,7 @@
 
 # bundle exec rake db:create
 # bundle exec rake db:migrate
-rake db:migrate 2>/dev/null || rake db:setup
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 
 rm -f tmp/pids/server.pid 
 bundle exec rails s -p $PORT -b '0.0.0.0'
