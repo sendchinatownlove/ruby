4805ffe8ff7a535b98110f509d76bda9dc9bb6e2
Justin McKibben <justintmckibben@gmail.com>
2020-04-17 14:32:03 -0500

    Add hero image url and progress bar color

--- spec/factories/seller.rb
+++ spec/factories/seller.rb
@@ -7,6 +7,8 @@ FactoryBot.define do
     story { Faker::Movies::StarWars.wookiee_sentence }
     owner_name { Faker::Movies::StarWars.character }
     owner_image_url { Faker::Lorem.word }
+    hero_image_url { Faker::Alphanumeric.alphanumeric(number: 64) }
+    progress_bar_color { Faker::Alphanumeric.alphanumeric(number: 64) }
     accept_donations { Faker::Boolean.boolean }
     sell_gift_cards { Faker::Boolean.boolean }
   end
