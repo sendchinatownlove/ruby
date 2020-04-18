f4175478d8d9633215fcc325710aa1cc133d1d56
Julie Xia <juliexxia@.google.com>
2020-04-18 12:42:09 -0400

    validate founding year

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,6 +2,7 @@ class Seller < ApplicationRecord
   # model association
   has_many :locations, dependent: :destroy
 
+  validates :founded_year, :inclusion => 1800..2020
   validates_uniqueness_of :seller_id
   validates_inclusion_of :sell_gift_cards, in: [true, false]
   validates_inclusion_of :accept_donations, in: [true, false]
