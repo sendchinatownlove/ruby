ea9a40b751b90bf3014e95db097b6166ee7fa5a6
Julie Xia <juliexxia@.google.com>
2020-04-18 17:48:19 -0400

    fix charges spec

--- spec/requests/charges_spec.rb
+++ spec/requests/charges_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Charges API', type: :request do
   # Test suite for POST /charges
   describe 'POST /charges' do
     let(:email) { 'mrkrabs@thekrustykrab.com' }
+    let(:nonce) {}
     let(:params) { { email: email, nonce: nonce, line_items: line_items } }
     let(:seller_id) { 'shunfa-bakery' }
     let!(:seller) { create(:seller, seller_id: seller_id, name: 'Shunfa Bakery') }
