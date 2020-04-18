91ab98fe52500d2f50f0f9beee7a2ccaa2f9fc6a
Justin McKibben <justintmckibben@gmail.com>
2020-04-18 15:44:09 -0500

    Update use_square logic to match .env

--- spec/factories/payment_intent.rb
+++ spec/factories/payment_intent.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :payment_intent do
     email { Faker::Internet.email }
-    stripe_id { Faker::Alphanumeric.alphanumeric(number: 64) if ENV['USE_STRIPE'].present? }
-    square_payment_id { Faker::Alphanumeric.alphanumeric(number: 64) if ENV['USE_SQUARE'].present? }
+    stripe_id { Faker::Alphanumeric.alphanumeric(number: 64) unless ENV['USE_SQUARE'] == 'true' }
+    square_payment_id { Faker::Alphanumeric.alphanumeric(number: 64) if ENV['USE_SQUARE'] == 'true' }
+    square_location_id { Faker::Alphanumeric.alphanumeric(number: 64) if ENV['USE_SQUARE'] == 'true' }
     successful { false }
   end
 end
