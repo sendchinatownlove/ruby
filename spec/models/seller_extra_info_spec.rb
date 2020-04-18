ae936d4d2eb471f9e1eeb100018e0555897c5e52
Julie Xia <juliexxia@.google.com>
2020-04-18 11:04:56 -0400

    fix test, add factory

--- spec/models/seller_extra_info_spec.rb
+++ spec/models/seller_extra_info_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SellerExtraInfo, type: :model do
-  it { should belong_to(:seller) }
+  let(:seller_extra_info1) do
+    create(
+      :seller_extra_info,
+      seller: seller1
+    )
+  end
+
+  it 'sucessfully creates' do
+    expect { seller_extra_info1.should belong_to(:seller) }
+  end
 end
