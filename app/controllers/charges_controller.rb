e4ab7c3656f7607d5cfd7bb002b4cf9556c08a9d
Julie Xia <juliexxia@.google.com>
2020-04-23 22:24:09 -0400

    fix

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -52,7 +52,7 @@ class ChargesController < ApplicationController
     params.require(:is_square)
     params.require(:nonce) if params[:is_square]
     params.require(:name)
-    params.permit(:email, :nonce, :is_square, line_items: [[:amount, :currency, :item_type, :quantity, :seller_id]], :name)
+    params.permit(:email, :nonce, :is_square, :name, line_items: [[:amount, :currency, :item_type, :quantity, :seller_id]])
   end
 
   def validate(line_item:)
