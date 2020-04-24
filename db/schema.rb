cc07593bc90cba965f151148443001764cb4f034
Julie Xia <juliexxia@.google.com>
2020-04-23 22:56:49 -0400

    oops

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_04_24_024902) do
-=======
-ActiveRecord::Schema.define(version: 2020_04_23_234913) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
