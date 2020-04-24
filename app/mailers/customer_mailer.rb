42bb3ab363467822ec88d66f54fabbaba639b1c9
Julie Xia <juliexxia@.google.com>
2020-04-23 22:21:40 -0400

    stuff

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,9 +1,8 @@
 class CustomerMailer < ApplicationMailer
   def send_receipt
     @payment_intent = params[:payment_intent]
-    @url = @payment_intent.receipt_url
+    @receipt_url = @payment_intent.receipt_url
     @note = @payment_intent.email_text
-    @customer_name = params[:customer_name]
 
     mail(to: @payment_intent.email, subject: 'Receipt from Send Chinatown Love')
   end
