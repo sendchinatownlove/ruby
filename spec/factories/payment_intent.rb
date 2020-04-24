157fb84cede86da0790e41e9be05d42df30e24b5
Julie Xia <juliexxia@.google.com>
2020-04-23 23:32:32 -0400

    update factory

--- spec/factories/payment_intent.rb
+++ spec/factories/payment_intent.rb
@@ -4,6 +4,9 @@ FactoryBot.define do
     stripe_id { Faker::Alphanumeric.alphanumeric(number: 64) unless ENV['USE_SQUARE'] == 'true' }
     square_payment_id { Faker::Alphanumeric.alphanumeric(number: 64) if ENV['USE_SQUARE'] == 'true' }
     square_location_id { Faker::Alphanumeric.alphanumeric(number: 64) if ENV['USE_SQUARE'] == 'true' }
+    receipt_url { Faker::Lorem.word }
+    name { Faker::Superhero.name }
+    email_text { Faker::Superhero.power }
     successful { false }
   end
 end
