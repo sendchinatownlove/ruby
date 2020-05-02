5a93df6e910dc51e7ffbbbbc6709b8e6914d33ab
Yong Su Lee <dkrcjs7521@gmail.com>
2020-05-02 02:27:49 -0400

    Add database restriction on square attributes

--- app/models/payment_intent.rb
+++ app/models/payment_intent.rb
@@ -2,6 +2,6 @@
 
 class PaymentIntent < ApplicationRecord
   validates_presence_of(:square_payment_id, :square_location_id)
-  validates_uniqueness_of :square_payment_id, allow_nil: true
+  validates_uniqueness_of :square_payment_id, allow_nil: false
   has_many :items
 end
