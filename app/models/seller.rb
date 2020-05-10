3b92b723bea36a9fa797c64ff76648af0587f9ed
Yong Su Lee <dkrcjs7521@gmail.com>
2020-05-10 16:22:06 -0400

    Add recent_contribution controller (#94)
    * Add item controller

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -38,6 +38,7 @@ class Seller < ApplicationRecord
   # model association
   has_many :locations, dependent: :destroy
   has_many :menu_items, dependent: :destroy
+  has_many :items, dependent: :destroy
 
   validates_presence_of :seller_id
   validates_presence_of :square_location_id
