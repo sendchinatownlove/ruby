f4cb1529e0fd45aad8311086c8c4864af18ec0d5
Yong Su Lee <dkrcjs7521@gmail.com>
2020-05-14 21:42:19 -0400

    Make `accept_donations` consistent throughout test case

--- spec/factories/seller.rb
+++ spec/factories/seller.rb
@@ -17,7 +17,7 @@ FactoryBot.define do
     owner_image_url { Faker::Lorem.word }
     hero_image_url { Faker::Alphanumeric.alphanumeric(number: 64) }
     progress_bar_color { Faker::Alphanumeric.alphanumeric(number: 64) }
-    accept_donations { Faker::Boolean.boolean }
+    accept_donations { false }
     sell_gift_cards { Faker::Boolean.boolean }
     business_type { Faker::Company.type }
     num_employees { Faker::Number.digit }
