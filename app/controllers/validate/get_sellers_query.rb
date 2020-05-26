ca175319b6a0c742aa506ffccffe7cc357850ec9
Yong Su Lee <dkrcjs7521@gmail.com>
2020-05-25 20:01:27 -0400

    Add GetSellersQuery validation PORO

---
+++ app/controllers/validate/get_sellers_query.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Validate
+  class GetSellersQuery
+    include ActiveModel::Validations
+
+    attr_accessor :sort_key, :sort_order
+
+    validates :sort_key, presence: true, inclusion: { in: %w[created_at] }
+    validates :sort_order, presence: true, inclusion: { in: %w[desc asc] }
+
+    def initialize(params = {})
+      if params[:sort].nil?
+        # Default to most recent sellers
+        @sort_key = 'created_at'
+        @sort_order  = 'desc'
+      else
+        @sort_key = params[:sort].split('.')[0]
+        @sort_order = params[:sort].split('.')[1] || 'desc'
+      end
+    end
+  end
+end
