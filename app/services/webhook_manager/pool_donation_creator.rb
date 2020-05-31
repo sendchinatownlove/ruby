0788b672ed1154c6d5a8523c49199f21def33d63
Yong Su Lee <dkrcjs7521@gmail.com>
2020-05-30 21:16:04 -0400

    Gift card creator for not creating duplicate gift cards

---
+++ app/services/webhook_manager/pool_donation_creator.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# Creates donation and item with the corresponding payload
+module WebhookManager
+  class PoolDonationCreator < BaseService
+    attr_reader :seller_id, :payment_intent, :amount
+
+    def initialize(params)
+      @seller_id = params[:seller_id]
+      @payment_intent = params[:payment_intent]
+      @amount = params[:amount]
+    end
+
+    def call
+      ActiveRecord::Base.transaction do
+        # TODO(jtmckibb): This is a very inefficient sort, since each time we
+        #                 call amount_raised, it has to fetch all of the
+        #                 associated Items, DonationDetails, and GiftCardDetails
+        #                 Then for each GiftCardDetail, it has to fetch every
+        #                 amount in an N+1 query, then sum everything. This is
+        #                 all in an N log N sort—which is horrible. Ideally,
+        #                 we would memoize amount_raised, and fix the N+1 query
+        #                 in GiftCardDetail that calculates amount.
+        @donation_sellers = Seller.filter_by_accepts_donations.sort_by do |s|
+          s.amount_raised
+        end
+
+        # calculate amount per merchant
+        # This will break if we ever have zero merchants but are still
+        # accepting pool donations.
+        amount_per = (amount.to_f / @donation_sellers.count.to_f).floor
+        remainder = amount % @donation_sellers.count
+
+        donations = @donation_sellers.map do |seller|
+          next if seller.seller_id.eql?(Seller::POOL_DONATION_SELLER_ID)
+
+          item = WebhookManager::ItemCreator.call({
+            item_type: :donation,
+            seller_id: seller.seller_id,
+            payment_intent: payment_intent
+          })
+
+          donation = DonationDetail.create!(
+            item: item,
+            amount: amount_per + (remainder > 0 ? 1 : 0)
+          )
+
+          remainder -= 1
+
+          donation
+        end
+
+        payment_intent.successful = true
+        payment_intent.save!
+
+        donations
+      end
+    end
+  end
+end
---
+++ spec/services/webhook_manager/gift_card_creator_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe WebhookManager::GiftCardCreator, '#call' do
+  let(:seller) { create :seller }
+
+  it 'creates gift_card_details' do
+    payment_intent = create :payment_intent
+
+    payload = new_payload(payment_intent)
+    gift_card_details = WebhookManager::GiftCardCreator.call(payload)
+
+    item = Item.last
+
+    expect(item.seller).to eq(seller)
+    expect(item.purchaser).to eq(payment_intent.purchaser)
+    expect(item.item_type).to eq('gift_card')
+    expect(gift_card_details.item).to eq(item)
+    expect(gift_card_details.amount).to eq(payload[:amount])
+    expect(gift_card_details.recipient).to eq(payment_intent.recipient)
+    expect(PaymentIntent.last.successful).to be true
+  end
+
+  it 'does not create gift_card_details due to stale data' do
+    create :payment_intent
+
+    payment_intent_1 = PaymentIntent.last
+    payment_intent_2 = PaymentIntent.last
+
+    payload = new_payload(payment_intent_1)
+
+    gift_card_details = WebhookManager::GiftCardCreator.call(payload)
+
+    item = Item.last
+    count = Item.count
+
+    expect(item.seller).to eq(seller)
+    expect(item.purchaser).to eq(payment_intent_1.purchaser)
+    expect(item.item_type).to eq('gift_card')
+    expect(gift_card_details.item).to eq(item)
+    expect(gift_card_details.amount).to eq(payload[:amount])
+    expect(gift_card_details.recipient).to eq(payment_intent_1.recipient)
+    expect(PaymentIntent.last.successful).to be true
+
+    payload = new_payload(payment_intent_2)
+
+    expect{WebhookManager::GiftCardCreator.call(payload)}.
+      to raise_error(ActiveRecord::StaleObjectError)
+
+    expect(count).to eq(Item.count)
+  end
+
+  def new_payload(payment_intent)
+    {
+      seller_id: seller.seller_id,
+      payment_intent: payment_intent,
+      amount: 50,
+      single_use: true
+    }
+  end
+end
