59bb9d4e4e66958054dffa5884720b582a3b5cfe
Julie Xia <juliexxia@google.com>
2020-06-17 21:46:11 -0400

    add comment about bad migration

--- db/migrate/20200617023402_add_delivery_type_key_to_delivery_option.rb
+++ db/migrate/20200617023402_add_delivery_type_key_to_delivery_option.rb
@@ -1,5 +1,6 @@
 class AddDeliveryTypeKeyToDeliveryOption < ActiveRecord::Migration[6.0]
   def change
+  	# This should have been :delivery_option
   	add_reference :delivery_types, :delivery_options, index:true
   end
 end
