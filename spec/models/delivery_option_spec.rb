4f94e8b87411719bda907a0867f57efae7937e74
Julie Xia <juliexxia@google.com>
2020-06-17 22:29:09 -0400

    explicitly create in specs

--- spec/models/delivery_option_spec.rb
+++ spec/models/delivery_option_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DeliveryOption, type: :model do
+	let!(:delivery_option) do
+		create(:delivery_option)
+	end
+
 	it { should have_one(:delivery_type) }
 	it { should belong_to(:seller) }
 end
--- spec/models/delivery_type_spec.rb
+++ spec/models/delivery_type_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DeliveryType, type: :model do
+	let!(:delivery_type) do
+		create(:delivery_type)
+	end
+
 	it { should validate_presence_of(:name) }
 	it { should validate_uniqueness_of(:name) }
 end
