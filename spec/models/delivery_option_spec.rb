a7698a207a388eaa0c10107a495a61f8ef5ca6a0
Julie Xia <juliexxia@google.com>
2020-06-17 21:58:21 -0400

    some tests

--- spec/models/delivery_option_spec.rb
+++ spec/models/delivery_option_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe DeliveryOption, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it { should have_one(:delivery_type) }
+	it { should belong_to(:seller) }
 end
