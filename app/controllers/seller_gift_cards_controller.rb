78919455fff605b43a76f9def3c8cbbef8fbe61c
Justin McKibben <justintmckibben@gmail.com>
2020-07-25 14:10:33 -0500

    Add seller gift cards endpoint (#175)
    * Add seller gift cards access token
    
    * Add seller gift cards endpoint
    
    * Run rubocop

---
+++ app/controllers/seller_gift_cards_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class SellerGiftCardsController < ApplicationController
+  before_action :set_seller
+
+  # GET /sellers/:seller_id/gift_cards/:id
+  def show
+    query = GiftCardDetail
+            .select(
+              :seller_gift_card_id,
+              :value,
+              :name,
+              :email,
+              :created_at,
+              :expiration
+            )
+            .joins(:item, :recipient)
+            .where(
+              items: {
+                seller_id: @seller.id,
+                refunded: false
+              }
+            )
+            .joins(
+              "join (#{GiftCardAmount.latest_amounts_sql}) as la on la.gift_card_detail_id = gift_card_details.id"
+            )
+            .to_sql
+
+    # processing in one query to get a PG::Result, instead multiple queries when building html
+    result = GiftCardDetail.connection.select_all(query)
+    json_response(result)
+  end
+
+  private
+
+  def set_seller
+    @seller = Seller.find_by!(seller_id: params[:seller_id])
+
+    if @seller.gift_cards_access_token != params[:id]
+      raise ActiveRecord::RecordNotFound
+    end
+  end
+end
