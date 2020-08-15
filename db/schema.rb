de1be826d89e11679bf162c53963e75a58ef1357
Dan Deng <danthaman@fb.com>
2020-08-14 21:37:55 -0400

    Add borough field to Location model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_172026) do
+ActiveRecord::Schema.define(version: 2020_08_15_013131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 2020_08_14_172026) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone_number"
     t.string "neighborhood"
+    t.string "borough"
     t.index ["seller_id"], name: "index_locations_on_seller_id"
   end
 
