b074202bcdd0bbe621bc29ec4182a2223abe54eb
Rohan Jadvani <rohan@workos.com>
2020-09-12 15:07:43 -0400

    Add Lyft sponsored flag to participating sellers

---
+++ db/migrate/20200912190203_add_lyft_sponsored_flag_to_participating_seller.rb
@@ -0,0 +1,5 @@
+class AddLyftSponsoredFlagToParticipatingSeller < ActiveRecord::Migration[6.0]
+  def change
+    add_column :participating_sellers, :is_lyft_sponsored, :boolean, default: false
+  end
+end
