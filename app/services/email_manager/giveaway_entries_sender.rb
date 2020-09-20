461657d7906aaa0540a24d3a335785f947b91996
Jeremy Xue <jeremyxue@gmail.com>
2020-09-20 01:48:37 -0700

    update giveaway entries sender with new text for 0 entries

--- app/services/email_manager/giveaway_entries_sender.rb
+++ app/services/email_manager/giveaway_entries_sender.rb
@@ -13,6 +13,16 @@ module EmailManager
 
       number_of_entries = (number_of_tickets / 3).floor
 
+      if number_of_entries == 0
+        entry_status = "You're not entered into the giveaway yet, only #{3 - number_of_tickets} more tickets until your first entry!"
+      else
+        if number_of_tickets % 3 == 0
+          entry_status = "You have been entered into this week's giveaway!"
+        else
+          entry_status = "You have been entered into this week's giveaway! Only #{number_of_tickets % 3} more tickets until your next entry!"
+        end
+      end
+
       html = <<~EOF
         <!DOCTYPE html>
         <html>
@@ -20,7 +30,7 @@ module EmailManager
             <meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
           </head>
           <body>
-            <p>You have been entered into this week’s giveaway!</p>
+            <p>#{entry_status}</p>
             <p>
               Number of entries: #{number_of_entries}
             </p>
@@ -32,7 +42,7 @@ module EmailManager
             <p>
               Check out this week’s giveaway items at our <a href="https://www.sendchinatownlove.com/food-crawl.html" target="_blank">Food Crawl website</a> to see what you can win.
             </p>
-            <p>We will be announcing the winners on <a href="https://www.instagram.com/sendchinatownlove/" target="_blank">IG</a> Monday, Sep. 14th at 10PM EST!</p>
+            <p>We will be announcing the winners on <a href="https://www.instagram.com/sendchinatownlove/" target="_blank">IG</a> Monday, Sep. 21st at 10PM EST!</p>
           </body>
         </html>
       EOF
