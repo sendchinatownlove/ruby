7205987d1f853dcce4a7507bbe5626853c6b0633
Jeremy Xue <jeremyxue@gmail.com>
2020-11-07 19:53:28 -0800

    change charges_controller to fail with 422 instead of 404 if cannot find project_id

--- spec/requests/charges_spec.rb
+++ spec/requests/charges_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe 'Charges API', type: :request do
     let(:is_square) { true }
     let(:name) { 'Squarepants, Spongebob' }
     let(:seller_id) { 'shunfa-bakery' }
-    let(:project) { create(:project, name: 'Light Up Chinatown', square_location_id: ENV['SQUARE_LOCATION_ID']) }
     let(:idempotency_key) { '123' }
     let(:is_subscribed) { 'true' }
     let(:is_distribution) { false }
@@ -35,6 +34,7 @@ RSpec.describe 'Charges API', type: :request do
              name: 'Shunfa Bakery',
              cost_per_meal: 100)
     end
+    let!(:project) { create(:project, name: 'Light Up Chinatown', square_location_id: ENV['SQUARE_LOCATION_ID']) }
 
     let(:expected_line_items) do
       line_items.map { |li| li.except(:is_distribution) }
@@ -241,49 +241,6 @@ RSpec.describe 'Charges API', type: :request do
         end
       end
 
-      context 'with both a project_id and seller_id' do
-        let(:line_items) do
-          [
-            {
-              amount: 6000,
-              currency: 'usd',
-              item_type: 'donation',
-              quantity: 1,
-              project_id: project.id,
-              seller_id: seller_id,
-              is_distribution: is_distribution
-            }
-          ]
-        end
-
-        let(:params) do
-          {
-            email: email,
-            is_square: is_square,
-            nonce: nonce,
-            line_items: line_items,
-            project_id: project.id,
-            seller_id: seller_id,
-            name: name,
-            idempotency_key: idempotency_key,
-            is_subscribed: is_subscribed,
-            is_distribution: is_distribution
-          }
-        end
-
-        before { post '/charges', params: params, as: :json }
-
-        it 'returns status code 422' do
-          expect(response).to have_http_status(422)
-        end
-
-        it 'returns a validation failure message' do
-          expect(response.body).to match(
-            /Project or Seller must exist, but not both. seller id: #{seller_id}, project_id: #{project.id}/
-          )
-        end
-      end
-
       context 'with gift card donation for distribution' do
         let(:line_items) do
           [
@@ -438,6 +395,131 @@ RSpec.describe 'Charges API', type: :request do
       it 'returns a validation failure message' do
         expect(response.body).to match(
           /Project or Seller must exist, but not both. seller id: , project_id:/
+        )
+      end
+    end
+
+    context 'with charge with both a project_id and seller_id' do
+      let(:line_items) do
+        [
+          {
+            amount: 6000,
+            currency: 'usd',
+            item_type: 'donation',
+            quantity: 1,
+            project_id: project.id,
+            seller_id: seller_id,
+            is_distribution: is_distribution
+          }
+        ]
+      end
+
+      let(:params) do
+        {
+          email: email,
+          is_square: is_square,
+          nonce: nonce,
+          line_items: line_items,
+          project_id: project.id,
+          seller_id: seller_id,
+          name: name,
+          idempotency_key: idempotency_key,
+          is_subscribed: is_subscribed,
+          is_distribution: is_distribution
+        }
+      end
+
+      before { post '/charges', params: params, as: :json }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to match(
+          /Project or Seller must exist, but not both. seller id: #{seller_id}, project_id: #{project.id}/
+        )
+      end
+    end
+
+    context 'with an non-existant seller_id' do
+      let(:line_items) do
+        [
+          {
+            amount: 6000,
+            currency: 'usd',
+            item_type: 'donation',
+            quantity: 1,
+            seller_id: 'notarealsellerid',
+            is_distribution: is_distribution
+          }
+        ]
+      end
+
+      let(:params) do
+        {
+          email: email,
+          is_square: is_square,
+          nonce: nonce,
+          line_items: line_items,
+          seller_id: 'notarealsellerid',
+          name: name,
+          idempotency_key: idempotency_key,
+          is_subscribed: is_subscribed,
+          is_distribution: is_distribution
+        }
+      end
+
+      before { post '/charges', params: params, as: :json }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to match(
+          /Project or Seller must exist, but not both. seller id: notarealsellerid, project_id: /
+        )
+      end
+    end
+
+    context 'with an non-existant project_id' do
+      let(:line_items) do
+        [
+          {
+            amount: 6000,
+            currency: 'usd',
+            item_type: 'donation',
+            quantity: 1,
+            project_id: 'notarealprojectid',
+            is_distribution: is_distribution
+          }
+        ]
+      end
+
+      let(:params) do
+        {
+          email: email,
+          is_square: is_square,
+          nonce: nonce,
+          line_items: line_items,
+          project_id: 'notarealprojectid',
+          name: name,
+          idempotency_key: idempotency_key,
+          is_subscribed: is_subscribed,
+          is_distribution: is_distribution
+        }
+      end
+
+      before { post '/charges', params: params, as: :json }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to match(
+          /Project or Seller must exist, but not both. seller id: , project_id: notarealprojectid/
         )
       end
     end
