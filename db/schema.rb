1e9297b70591436f0a52268cd61d8c896f19e61e
Ryan Namgung <ryannamgung@Ryans-MacBook-Pro.local>
2020-11-09 18:01:39 -0500

    edited the distributors schema by adding tokens and using rails db commands

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_031517) do
+ActiveRecord::Schema.define(version: 2020_11_09_225459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,9 @@ ActiveRecord::Schema.define(version: 2020_10_30_031517) do
   create_table "delivery_options", force: :cascade do |t|
     t.string "url"
     t.string "phone_number"
+    t.bigint "seller_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "seller_id", null: false
     t.index ["seller_id"], name: "index_delivery_options_on_seller_id"
   end
 
@@ -68,6 +68,9 @@ ActiveRecord::Schema.define(version: 2020_10_30_031517) do
     t.string "image_url"
     t.bigint "contact_id"
     t.string "name"
+    t.string "gift_card_login_token"
+    t.datetime "gift_card_login_expires_at"
+    t.string "gift_card_login_state", default: "new", null: false
     t.index ["contact_id"], name: "index_distributors_on_contact_id"
   end
 
