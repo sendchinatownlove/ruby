d59a01b3bd00b20d31dfff6a48db9b8baf54ae10
Chris Ng <chris.ng93@gmail.com>
2020-11-11 18:55:15 -0800

    simple tests

--- spec/models/campaigns_sellers_distributor_spec.rb
+++ spec/models/campaigns_sellers_distributor_spec.rb
@@ -24,5 +24,7 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsSellersDistributor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:campaign) }
+  it { should belong_to(:seller) }
+  it { should belong_to(:distributor) }
 end
