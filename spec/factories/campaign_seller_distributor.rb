b8dfad3fa3087db92cbcfd57cf6e35b42c81a8ee
Chris Ng <chris.ng93@gmail.com>
2020-11-11 22:54:58 -0800

    rm

--- spec/factories/campaign_seller_distributor.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-    factory :campaign_seller_distributor do
-        association :campaign
-        association :distributor
-        association :seller
-    end
-end
