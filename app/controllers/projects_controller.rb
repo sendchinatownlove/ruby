63a7afd0cebc5c996b97cb6c4888ca98a517387d
Justin McKibben <justintmckibben@gmail.com>
2020-11-12 17:16:57 -0800

    Add projects endpoint
    Also add amount_raised to model and amount to
    payment_intent model

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,24 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show update]
+
+  # GET /projects/:id
+  def show
+    json_response(project_json)
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def projects_json
+    @projects.map { |c| project_json project: c }
+  end
+
+  def project_json(project: @project)
+    ret = project.as_json
+    ret['amount_raised'] = project.amount_raised
+    ret
+  end
+end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,4 +14,57 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   it { should validate_presence_of(:square_location_id) }
+
+  let!(:project) do
+    create(:project)
+  end
+
+  let(:line_items) do
+    [
+      {
+        amount: 6000,
+        currency: 'usd',
+        item_type: 'donation',
+        quantity: 1,
+        project_id: project.id,
+        seller_id: seller_id,
+        is_distribution: is_distribution
+      }
+    ]
+  end
+
+  describe '#amount_raised' do
+    before do
+        create(:payment_intent, :with_project, :with_line_items, successful: true)
+        create(:payment_intent, :with_project, :with_line_items, successful: true)
+        create(:payment_intent, :with_project, :with_line_items, successful: false)
+        create(:payment_intent, :with_line_items, successful: true)
+    end
+
+    context 'with no payment intents' do
+      it 'should return zero as amount_raised' do
+        expect(project.amount_raised).to eq 0
+      end
+    end
+
+    context 'with successful payment intents' do
+      before do
+        create(:payment_intent, :with_line_items, project: project, successful: true)
+        create(:payment_intent, :with_line_items, project: project, successful: true)
+      end
+      it 'should return amount' do
+        expect(project.amount_raised).to eq 1200
+      end
+    end
+
+    context 'with no successful payment intents' do
+      before do
+        create(:payment_intent, :with_line_items, project: project, successful: false)
+      end
+
+      it 'should return zero as amount_raised' do
+        expect(project.amount_raised).to eq 0
+      end
+    end
+  end
 end
