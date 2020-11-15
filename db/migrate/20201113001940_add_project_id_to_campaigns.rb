30564472851f6de31c7c3414b1466c7638575e59
Jeremy Xue <jeremyxue@gmail.com>
2020-11-15 10:08:05 -0800

    Add project_id to campaigns for Mega-GAM (#286)
    * make seller_id nullable in campaigns
    
    * Add project_id to campaigns
    
    * add project_id checks to campaign model and controller
    
    * add check to controller and add test for project_id + seller_id in campaign controller
    
    * add tests for both project and seller for campaigns
    
    Co-authored-by: Alice <squeeshed@gmail.com>

---
+++ db/migrate/20201113001940_add_project_id_to_campaigns.rb
@@ -0,0 +1,7 @@
+class AddProjectIdToCampaigns < ActiveRecord::Migration[6.0]
+  def change
+    add_column :campaigns, :project_id, :bigint, null: true
+    add_index :campaigns, :project_id
+    add_foreign_key :campaigns, :projects
+  end
+end
