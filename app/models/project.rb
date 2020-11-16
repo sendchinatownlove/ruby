8910512a3b7921996cb73acc55f8a17fb01d98f0
Justin McKibben <justintmckibben@gmail.com>
2020-11-15 18:13:39 -0800

    Don't return refunded payment intents for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,6 +14,10 @@ class Project < ApplicationRecord
   validates_presence_of :square_location_id
 
   def amount_raised
-    PaymentIntent.where(project_id: id, successful: true).map(&:amount).sum
+    refunded_payment_intent_ids = Refund.where(status: :COMPLETED)
+      .map(&:payment_intent_id)
+    PaymentIntent.where(project_id: id, successful: true)
+      .where.not(id: refunded_payment_intent_ids)
+      .map(&:amount).sum
   end
 end
