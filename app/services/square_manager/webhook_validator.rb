ddce68d40b8a3258cfa111a7da764cd42ebca8af
Lance Sun <lancesun22@gmail.com>
2020-12-17 18:05:49 -0500

    Refactor webhook hashing

--- app/services/square_manager/webhook_validator.rb
+++ app/services/square_manager/webhook_validator.rb
@@ -17,34 +17,22 @@ module SquareManager
 
       # Generate the HMAC-SHA1 signature of the string, signed with your webhook
       # signature key
-      string_signature = Base64.strict_encode64(
-        OpenSSL::HMAC.digest(
-          'sha1',
-          ENV['SQUARE_WEBHOOK_SIGNATURE_KEY'],
-          string_to_sign
+      accounts = %w[square think_chinatown apex]
+      signatures = accounts.map do |account_name|
+        Base64.strict_encode64(
+          OpenSSL::HMAC.digest(
+            'sha1',
+            ENV["#{account_name.upcase}_WEBHOOK_SIGNATURE_KEY"],
+            string_to_sign
+          )
         )
-      )
-
-      # we also have webhooks request that could come from the Think!Chinatown account
-      think_chinatown_signature = Base64.strict_encode64(
-        OpenSSL::HMAC.digest(
-          'sha1',
-          ENV['THINK_CHINATOWN_WEBHOOK_SIGNATURE_KEY'],
-          string_to_sign
-        )
-      )
-
-      apex_signature = Base64.strict_encode64(
-        OpenSSL::HMAC.digest(
-          'sha1',
-          ENV['APEX_WEBHOOK_SIGNATURE_KEY'],
-          string_to_sign
-        )
-      )
+      end
 
       # Hash the signatures a second time (to protect against timing attacks)
       # and compare them
-      unless [Digest::SHA1.base64digest(string_signature), Digest::SHA1.base64digest(think_chinatown_signature), Digest::SHA1.base64digest(apex_signature)].include?(Digest::SHA1.base64digest(callback_signature))
+      hashed_signatures = signatures.map { |signature| Digest::SHA1.base64digest(signature) }
+
+      unless hashed_signatures.include?(Digest::SHA1.base64digest(callback_signature))
         raise ExceptionHandler::InvalidSquareSignature
       end
     end
