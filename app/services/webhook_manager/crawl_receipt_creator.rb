15020259a8554cdd22e762271da9df3d3d5f26aa
Jeremy Xue <jeremyxue@gmail.com>
2021-01-26 19:23:58 -0800

    crawl receipt creator service class

---
+++ app/services/webhook_manager/crawl_receipt_creator.rb
@@ -0,0 +1,17 @@
+
+module WebhookManager
+   class CrawlReceiptCreator < BaseService
+    attr_reader :payment_intent, :amount, :contact_id, :payment_intent_id
+
+    def initialize(params)
+      @payment_intent = params[:payment_intent]
+      @amount = params[:amount]
+      @contact_id = payment_intent.purchaser_id
+      @payment_intent_id = payment_intent.id
+    end
+
+    def call
+      CrawlReceipt.create!(amount: amount, payment_intent_id: payment_intent_id, contact_id: contact_id, receipt_url: nil)
+    end
+  end
+end
