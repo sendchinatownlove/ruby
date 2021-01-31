fb20d380e88b9c626bab7711d5f02db08d024074
Jeremy Xue <jeremyxue@gmail.com>
2021-01-30 16:27:47 -0800

    add migration

---
+++ db/migrate/20210129084819_add_active_to_participating_sellers.rb
@@ -0,0 +1,7 @@
+class AddActiveToParticipatingSellers < ActiveRecord::Migration[6.0]
+  def change
+    change_table(:participating_sellers) do |t|
+      t.column :active, :boolean, default: true
+    end
+  end
+end
