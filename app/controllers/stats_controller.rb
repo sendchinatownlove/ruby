5f9c119acfdce82af4128b33393b03e78a7814f6
stanley zheng <stanley@zheng.nyc>
2021-04-05 20:12:33 -0400

    create named attributes and format constants

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,14 +5,17 @@ require_relative 'stats_helpers/stats_html.rb'
 
 class StatsController < ApplicationController
     def donation_totals
-        return ActiveRecord::Base.connection.execute($donation_query)
-      end
+        outside_db_contributions = 123572
+        query = ActiveRecord::Base.connection.execute($donation_query)
+        return query.getvalue(0,5) + outside_db_contributions 
+    end
 
     def gam_count
         @GiftCardDetail = GiftCardDetail.where("single_use": true)
         # Magic constant to track orders not in GAM 
+        outside_db_contributions = 5140
         # TODO(stanzheng) pull magic constant from GAM spreadsheet
-        return @GiftCardDetail.length + 5140
+        return @GiftCardDetail.length + outside_db_contributions
     end 
     def index
         show(donation_totals(), sellers_total(), transaction_totals(), gam_count()) 
@@ -27,12 +30,12 @@ class StatsController < ApplicationController
     end
     
     def show(donation_totals, sellers_total, transaction_totals, gam_count)
-        box1 =  "$%s" % ActionController::Base.helpers.number_with_precision( donation_totals.getvalue(0,5), :precision => 0, :delimiter => ',')  #"$10,000"
-        box2 =  ActionController::Base.helpers.number_with_precision( gam_count, :precision => 0, :delimiter => ',') 
-        box3 = "36,573" 
-        box4 = ActionController::Base.helpers.number_with_precision( transaction_totals, :precision => 0, :delimiter => ',') 
-        box5 = sellers_total
-        box6 = "$47,689"
-        render html: stats_html(box1, box2, box3, box4, box5, box6).html_safe# render :text => @model_object.html_content
+        donation_totals     = "$%s" % ActionController::Base.helpers.number_with_precision( donation_totals, :precision => 0, :delimiter => ',')  #"$10,000"
+        gam_count           = ActionController::Base.helpers.number_with_precision( gam_count, :precision => 0, :delimiter => ',') 
+        foodcrawl_raised    = "36,573" 
+        transaction_totals  = ActionController::Base.helpers.number_with_precision( transaction_totals, :precision => 0, :delimiter => ',') 
+        sellers_total
+        luc_raised          = "$47,689"
+        render html: stats_html(donation_totals, gam_count, foodcrawl_raised, transaction_totals, sellers_total, luc_raised).html_safe# render :text => @model_object.html_content
     end
 end
