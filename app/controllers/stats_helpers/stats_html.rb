913afe1f6c6c30da5e3d176512163918a11cb03a
stanley zheng <stanley@zheng.nyc>
2021-04-07 22:28:00 -0400

    updating update code

--- app/controllers/stats_helpers/stats_html.rb
+++ app/controllers/stats_helpers/stats_html.rb
@@ -3,32 +3,32 @@ def stats_html(box1, box2, box3, box4, box5, box6)
     return <<-HTML.chomp
     <div style="display: flex; flex-wrap: wrap; justify-content: center; font-family: 'Open Sans', sans-serif; color: white; font-weight:bold;">
     <div style="display: flex; flex-direction: column; align-items: center; width: 30%; margin: 30px 0px;">
-        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox1">#{box1}</div>
+        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox1">$516,051</div>
         <div style="font-size: 16px;">Raised</div>
     </div>
 
     <div style="display: flex; flex-direction: column; align-items: center; width: 30%; margin: 30px 0px;">
-        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox2">#{box2}</div>
+        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox2">13,818</div>
         <div style="font-size: 16px;">Meals donated</div>
     </div>
 
     <div style="display: flex; flex-direction: column; align-items: center; width: 30%; margin: 30px 0px;">
-        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox3">#{box3}</div>
+        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox3">36,573</div>
         <div style="font-size: 16px;">Raised from 2 Food Crawls</div>
     </div>
 
     <div style="display: flex; flex-direction: column; align-items: center; width: 30%; margin: 30px 0px;">
-        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox4">#{box4}</div>
+        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox4">53,737</div>
         <div style="font-size: 16px;">Donations and vouchers purchased</div>
     </div>
 
     <div style="display: flex; flex-direction: column; align-items: center; width: 30%; margin: 30px 0px;">
-        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox5">#{box5}</div>
+        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox5">30</div>
         <div style="font-size: 16px;">Merchants directly supported</div>
     </div>
 
     <div style="display: flex; flex-direction: column; align-items: center; width: 30%; margin: 30px 0px;">
-        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox6">#{box6}</div>
+        <div style="font-size: 50px; margin: 15px 0px;" id="statsbox6">$47,689</div>
         <div style="font-size: 16px;">Raised for Light Up Chinatown</div>
     </div>
 </div>
@@ -40,10 +40,15 @@ function quotation(id, text) {
     // if (q) q.html().replace("", text);
 }
 
-fetch("https://ifconfig.me/all.json")
+fetch("https://sendchinatownlove.herokuapp.com/stats")
     .then(response => response.json())
-    .then(data => quotation('statsbox1', data["ip_addr"]));
-quotation('statsbox1', '200');
+    .then(data => {
+        Object.keys(data).forEach(function(key) {
+        quotation(`stats${key}`, data[key])
+})
+
+});
+
 </script>
             HTML
 end 
